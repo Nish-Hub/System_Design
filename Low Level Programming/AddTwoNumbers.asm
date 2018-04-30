70d68cdc34baa092ebe612059f2a56933110a51d
Half_Code <nishantgsingh07@gmail.com>
2018-04-30 07:33:39 +0530

    Added comment

--- Low Level Programming/AddTwoNumbers.asm
+++ Low Level Programming/AddTwoNumbers.asm
@@ -2,7 +2,7 @@
 // Add values of registers at address 0 and 1 and save it in register in address 2
 @R0  // Added symbols for registers whose value we are going to access
 D=M
-@R1
+@R1   //A register used as an address register not a data register like in D=A
 D=M+D
 @R2
 M=D
