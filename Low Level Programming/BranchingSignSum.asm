b9dd32eb427bb8fba410ce093e69f8d669d6c8f4
Half_Code <nishantgsingh07@gmail.com>
2018-04-30 08:43:18 +0530

    Added symbols in place of line numbers

--- Low Level Programming/BranchingSignSum.asm
+++ Low Level Programming/BranchingSignSum.asm
@@ -4,14 +4,16 @@
 // else r1=0
 @R0
 D=M
-@8  // Line number of code without any space
+@POSITIVE  // Line number of code without any space
 D ;JGT  //if R0 >0 , then goto line 8 or value of A //register is used as a line number
 @R1
 M=0
-@10
+@END
 0;JMP  // Jump to end of program
+(POSITIVE)
 @R1
 M=1
-@10   // Infinite loop for termination to avoid NOP attack
+(END)
+@END   // Infinite loop for termination to avoid NOP attack
 0;JMP
 
